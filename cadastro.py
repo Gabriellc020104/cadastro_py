f610e234f3842ab2b91116832205d6f1b53e27cb
Gabriellc020104 <101996217+Gabriellc020104@users.noreply.github.com>
2024-02-22 00:57:25 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,12 @@
+
+nome = input("Digite seu nome completo: ")
+idade = input("Digite sua idade: ")
+endereco = input("Digite seu endereço completo: ")
+telefone = input("Digite seu número de telefone: ")
+
+
+print("Seus dados cadastrais:")
+print("Nome completo: " + nome)
+print("Idade: " + idade)
+print("Endereço completo: " + endereco)
+print("Número de telefone: " + telefone)
